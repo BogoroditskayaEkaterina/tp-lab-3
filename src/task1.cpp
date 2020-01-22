1484ecff0407276e36fe62751824256c08b4daa2
Ekaterina <55396312+BogoroditskayaEkaterina@users.noreply.github.com>
2020-01-22 06:53:24 +0300

    Update task1.cpp

--- src/task1.cpp
+++ src/task1.cpp
@@ -2,19 +2,24 @@
 #include "Circle.h"
 
 
-double calcDelta()
+double calcDelta() 
 {
-	double rad = 6378100.0;
-	Circle Earth(rad);
-	Earth.setFerence(Earth.getFerence() + 1.0);
-	return (Earth.getRadius() - rad);
+	double earthRadius = 6378100.0;
+	double ropeLength = 1.0;
+	Circle Earth(earthRadius);
+	double length = Earth.getFerence() + ropeLength;
+	Earth.setFerence(length);
+	return Earth.getRadius() - earthRadius;
 }
 
 double calcCost()
 {
-	Circle Pool(3.0);
-	Circle Pool_road(4.0);
-	double cost_road = (Pool_road.getArea() - Pool.getArea()) * 1000.0;
-	double cost_fence = Pool_road.getFerence() * 2000.0;
-	return cost_road + cost_fence;
+	double sqrmeterCost = 1000.0;
+	double meterCost = 2000.0;
+	Circle SwimmingPool(3.0);
+	double oldArea = SwimmingPool.getArea();
+	SwimmingPool.setRadius(4.0);
+	double roadPrice = (SwimmingPool.getArea() - oldArea) * sqrmeterCost;
+	double wallPrice = SwimmingPool.getFerence() * meterCost;
+	return roadPrice + wallPrice;
 }
